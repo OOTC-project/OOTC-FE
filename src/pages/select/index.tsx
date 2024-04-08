d33efe06b58d327ae61e292886f99ad2814f3ddd
junggwanhun <wjdrhksgns7602@gmail.com>
2024-04-08 23:27:26 +0900

    fix: change main page layout

--- src/pages/select/index.tsx
+++ src/pages/select/index.tsx
@@ -4,6 +4,8 @@ import {
   StyleSheet,
   ImageBackground,
   ScrollView,
+  Dimensions,
+  TouchableOpacity,
 } from 'react-native';
 import React, { useState } from 'react';
 import ItemSelectBox from '../../components/template/SelectBox';
@@ -18,13 +20,33 @@ const Select = () => {
         <View style={styles.marginTop} />
         <SelectImage />
         <View style={styles.mt}>
-          <ScrollView style={styles.scrollView}>
-            <ItemSelectBox title="Bookmark" />
+          <View style={styles.scrollView}>
+            {DATA.map(item => (
+              <View style={styles.boxBox} key={item.id}>
+                <TouchableOpacity style={styles.box}>
+                  <View style={styles.bb}>
+                    <View style={styles.bbb}>
+                      <Text>1</Text>
+                    </View>
+                    <View style={styles.bbb}>
+                      <Text>2</Text>
+                    </View>
+                    <View style={styles.bbb}>
+                      <Text>3</Text>
+                    </View>
+                  </View>
+                </TouchableOpacity>
+                <View style={styles.fontBox}>
+                  <Text style={styles.boxTitle}>{item.title}</Text>
+                </View>
+              </View>
+            ))}
+            {/* <ItemSelectBox title="Bookmark" />
             <ItemSelectBox title="OUTER" />
             <ItemSelectBox title="TOP" />
             <ItemSelectBox title="BOTTOM" />
-            <ItemSelectBox title="ETC" />
-          </ScrollView>
+            <ItemSelectBox title="ETC" /> */}
+          </View>
         </View>
       </ImageBackground>
     </View>
@@ -32,6 +54,7 @@ const Select = () => {
 };
 
 export default Select;
+const screenWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   container: {
@@ -43,8 +66,58 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     resizeMode: 'cover',
-    justifyContent: 'center',
   },
   mt: { marginTop: 10 },
-  scrollView: {},
+  scrollView: { width: screenWidth, flexDirection: 'row', flexWrap: 'wrap' },
+  boxBox: {
+    display: 'flex',
+    alignItems: 'center',
+    width: screenWidth / 4,
+    flexDirection: 'column',
+  },
+  box: {
+    backgroundColor: '#000',
+    opacity: 0.7,
+    marginTop: 15,
+    marginHorizontal: 10,
+    width: '80%',
+    height: screenWidth / 5,
+    borderRadius: 13,
+    padding: 8,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  fontBox: {
+    width: '80%',
+  },
+  boxTitle: { fontSize: 16, textAlign: 'center', fontWeight: 'bold' },
+
+  bb: {
+    justifyContent: 'space-between',
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  bbb: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '30%',
+    height: screenWidth / 5 / 3 - 8,
+    backgroundColor: 'blue',
+    borderRadius: 5,
+    flexDirection: 'column',
+  },
 });
+const DATA = [
+  { id: 0, title: 'Bookmark' },
+  { id: 1, title: 'Outer' },
+  { id: 2, title: 'Top' },
+  { id: 3, title: 'Bottom' },
+  { id: 4, title: 'Hat' },
+  { id: 5, title: 'shoes' },
+  { id: 6, title: 'Accessories' },
+  { id: 7, title: 'Bag' },
+  { id: 8, title: 'Socks' },
+  { id: 9, title: 'Glasses' },
+];
